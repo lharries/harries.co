2699e5c13996f4d6a86cdca72fb610b0586dc047
Luke Harries <github@lukeharries.me>
2020-04-03 16:32:50 +0100

    added all posts link

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -12,7 +12,7 @@ const BlogPostTemplate = ({ data, pageContext }) => {
 
   return (
     <Layout>
-      <SEO title={post.frontmatter.title} description={post.frontmatter.description || post.excerpt} />
+      <SEO title={post.frontmatter.title} description={post.frontmatter.description || post.excerpt}/>
       <article>
         <header>
           <h1
@@ -35,14 +35,14 @@ const BlogPostTemplate = ({ data, pageContext }) => {
             {post.frontmatter.date}
           </p>
         </header>
-        <section dangerouslySetInnerHTML={{ __html: post.html }} />
+        <section dangerouslySetInnerHTML={{ __html: post.html }}/>
         <hr
           style={{
             marginBottom: rhythm(1)
           }}
         />
         <footer>
-          <Bio />
+          <Bio/>
         </footer>
       </article>
 
@@ -58,15 +58,18 @@ const BlogPostTemplate = ({ data, pageContext }) => {
         >
           <li>
             {previous &&
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>}
+            <Link to={previous.fields.slug} rel="prev">
+              ← {previous.frontmatter.title}
+            </Link>}
+          </li>
+          <li>
+            <a href="/blog">All posts</a>
           </li>
           <li>
             {next &&
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>}
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title} →
+            </Link>}
           </li>
         </ul>
       </nav>
