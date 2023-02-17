c78ced449c9ce0fc1b5662d2a76ffd119d6487c5
Luke Harries <lharries@users.noreply.github.com>
2023-02-17 13:23:41 +0000

    added hey email

--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 import Obfuscate from "react-obfuscate"
 
 const ContactMe = () => {
-  const em1 = "luke@"
+  const em1 = "hey@"
   const em2 = "harr"
   const em3 = "ies.co"
 
