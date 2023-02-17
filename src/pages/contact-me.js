bfa22ceeee94527511296cab9d0253f98d230682
Luke Harries <lharries@users.noreply.github.com>
2023-02-17 13:23:13 +0000

    updated email

--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -3,6 +3,12 @@ import Layout from "../components/layout"
 import Obfuscate from "react-obfuscate"
 
 const ContactMe = () => {
+  const em1 = "luke@"
+  const em2 = "harr"
+  const em3 = "ies.co"
+
+  const email = em1 + em2 + em3
+
   return (
     <Layout>
       <h2>Contact me</h2>
@@ -13,7 +19,7 @@ const ContactMe = () => {
         <a href="https://twitter.com/messages/compose?recipient_id=854833945258491905">
           send me a DM on Twitter
         </a>{" "}
-        or email me at <Obfuscate email="luke.harries@me.com" />
+        or email me at <Obfuscate email={email} />
       </p>
     </Layout>
   )
