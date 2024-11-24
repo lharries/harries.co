52af2494d34fa85ab8870df26d1416ad5b2a4856
Luke Harries <lharries@users.noreply.github.com>
2024-11-24 19:19:30 +0000

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,19 @@
+import type { MetadataRoute } from "next";
+import { getBlogPosts } from "../lib/blog";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getBlogPosts();
+  return [
+    {
+      url: "https://harries.co",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    ...posts.map((post) => ({
+      url: `https://harries.co/${post.slug}`,
+      lastModified: post.date || new Date(),
+      priority: 1,
+    })),
+  ];
+}
